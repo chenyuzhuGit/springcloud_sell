a99bd4007207473c6d07605fd004a1cbecc51213
chenyuzhu <chenyuzhu@qq.com>
2020-04-11 14:28:33 +0800

    提交

--- api-gateway/src/main/java/com/imooc/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/imooc/apigateway/ApiGatewayApplication.java
@@ -8,10 +8,11 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 //    @ConfigurationProperties("zuul")
+
 //    @RefreshScope
 //    public ZuulProperties zuulProperties() {
 //        return new ZuulProperties();
